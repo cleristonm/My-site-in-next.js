a07646b5c995cd8f9e7ad96799dec55de1f6dc3c
Clériston Martinelo <cleriston@gmail.com>
2021-04-08 22:14:23 -0300

    Refactoring Footer component

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,24 @@
 import React, { Component, Fragment } from "react";
 
-function Footer() {  
+function Footer({dateTimeDeploy}) {  
     return (
         <footer style={{ position: "absolute", bottom: 0, width:"100%" }} className="bg-light">
         <div className="container mx-auto my-2 text-muted">
-            <small>Last deploy: {(new Date()).toString()}</small>
+            <small>Last deploy: {dateTimeDeploy}</small>
         </div>
         </footer>
     );  
+}
+
+export async function getStaticProps(context) {
+    const dateTimeDeploy = (new Date()).toString();
+    console.log(dateTimeDeploy);
+    return {
+      props:{
+        dateTimeDeploy,
+      }
+    }
+  
 }
 
 export default Footer;
